cbc2eafb88e9e49a017cfbe53fb369f70e2b1ce1
annisazzuhra <azzuhra25@mhs.usk.ac.id>
2025-11-12 21:58:51 +0700

    taro include

---
+++ ProgramSistem_AlatLab/fungsi_admin.c
@@ -0,0 +1,9 @@
+#include "alat.h"
+
+//FUNGSI ADMIN
+void ADMIN_MENU(data alat[],int *count){
+    int pilihan1;
+    system("cls");
+    printf("------------------------------\n");
+    printf("\tSELAMAT DATANG\n");
+    do{
