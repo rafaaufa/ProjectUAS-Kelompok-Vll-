933bce22491eae07d9d2e951ab111a06341ecd85
annisazzuhra <azzuhra25@mhs.usk.ac.id>
2025-11-12 22:00:06 +0700

    taro printf

--- ProgramSistem_AlatLab/fungsi_admin.c
+++ ProgramSistem_AlatLab/fungsi_admin.c
@@ -7,3 +7,13 @@ void ADMIN_MENU(data alat[],int *count){
     printf("------------------------------\n");
     printf("\tSELAMAT DATANG\n");
     do{
+        printf("------------------------------\n");
+        printf("[1.] TAMBAH DATA\n");
+        printf("[2.] LIHAT DATA\n");
+        printf("[3.] EDIT DATA\n");
+        printf("[4.] HAPUS DATA\n");
+        printf("[5.] KELUAR\n");
+        printf("Silahkan Masukkan Pilihan : ");
+        scanf("%d",&pilihan1);
+        getchar();
+        printf("\n------------------------------\n");
