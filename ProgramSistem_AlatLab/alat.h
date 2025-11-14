3e36edaaf809ac5e7275f4ff2416090348aed0b7
rafaaufa <vicdhan@mhs.usk.ac.id>
2025-11-14 19:20:35 +0700

    tambh fungsi file

--- ProgramSistem_AlatLab/alat.h
+++ ProgramSistem_AlatLab/alat.h
@@ -21,5 +21,7 @@ void ADMIN_LIHATDATA(data alat[], int count);
 void ADMIN_EDITDATA(data alat[], int *count);
 void ADMIN_HAPUSDATA(data alat[], int *count);
 
+void SIMPAN_FILE(data alat[], int count);
+void TAMBAH_FILE(data alat[], int *count);
 
 #endif
