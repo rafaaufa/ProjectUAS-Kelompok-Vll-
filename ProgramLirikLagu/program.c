39de1c796b88fdeaf1737f72be566206e3e2ece0
baruLagi <riski346@mhs.usk.ac.id>
2025-11-14 20:52:47 +0700

    tambah code lagi

--- ProgramLirikLagu/program.c
+++ ProgramLirikLagu/program.c
@@ -11,7 +11,8 @@ void bersihkan_kata(char *kata) {
             sementara[j] = tolower(kata[i]);
             j++;
         }
-}
+    }
+    sementara[j] = '\0';
 int main(){
     char kata[200];
     FILE *file1 = fopen("lirik.txt", "r");
